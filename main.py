15461389a82bdc06f4483af045d39fc09f37c94e
Nutpupicha Arungornpasuruk <6510615096@cn330>
2025-12-10 17:20:36 +0700

    add file from feature/air

---
+++ main.py
@@ -0,0 +1,14 @@
+import typer
+from weatherthai.commands.forecast import forecast_command
+from weatherthai.commands.current import current_command
+from weatherthai.commands.air import air_command
+
+app = typer.Typer(help="weatherthai - Thailand weather CLI")
+
+
+app.command(name="forecast")(forecast_command)
+app.command(name="current")(current_command)
+app.command(name="air")(air_command)
+
+if __name__ == "__main__":
+    app()
